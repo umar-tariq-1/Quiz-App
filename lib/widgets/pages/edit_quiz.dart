412bf55e7b855bbf0c6e8360fccc7314af3a9f06
umart823 <umart823@gmail.com>
2024-08-16 18:23:29 +0500

    .

--- lib/widgets/pages/edit_quiz.dart
+++ lib/widgets/pages/edit_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/main.dart';
+import 'package:quiz_app/widgets/base/custom_elevated_button.dart';
 import 'package:quiz_app/widgets/base/navigation_drawer.dart';
 import 'package:quiz_app/widgets/compound/custom_form.dart';
 
@@ -13,135 +14,126 @@ class EditQuiz extends StatefulWidget {
 }
 
 class _EditQuizState extends State<EditQuiz> {
-  void _formSubmitted(data) {
+  final controllers = {
+    'question': TextEditingController(),
+    'answer': TextEditingController(),
+    'option1': TextEditingController(),
+    'option2': TextEditingController(),
+    'option3': TextEditingController()
+  };
+
+  void _formSubmitted(_) {
+    Map data = {};
+    List controllerKeys = controllers.keys.toList();
+    for (var i in controllerKeys) {
+      data[i] = controllers[i]!.text.trim();
+    }
+    for (var i in controllerKeys) {
+      controllers[i]!.clear(); // This loop clears all text fields
+    }
+    FocusManager.instance.primaryFocus?.unfocus();
     print(data);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(68),
-            child: Container(
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3.5),
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(68),
+          child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3.5),
+                ),
+              ]),
+              child: AppBar(
+                toolbarHeight: 68,
+                centerTitle: true,
+                title: const Text(
+                  "Edit Quiz",
+                  style: TextStyle(
+                    fontFamily: 'BeautifulPeople',
+                    fontSize: 25.25,
+                    letterSpacing: 1.3,
+                    wordSpacing: 1.2,
                   ),
-                ]),
-                child: AppBar(
-                  toolbarHeight: 68,
-                  centerTitle: true,
-                  title: const Text(
-                    "Edit Quiz",
-                    style: TextStyle(
-                      fontFamily: 'BeautifulPeople',
-                      fontSize: 25.25,
-                      letterSpacing: 1.3,
-                      wordSpacing: 1.2,
-                    ),
-                  ),
-                  leading: Builder(
-                    builder: (context) {
-                      return Container(
-                          margin: const EdgeInsets.only(left: 5),
-                          child: IconButton(
-                            icon: const Icon(Icons.menu),
-                            color: appBarTextColor,
-                            iconSize: 29,
-                            onPressed: () {
-                              Scaffold.of(context).openDrawer();
-                            },
-                          ));
-                    },
-                  ),
-                  backgroundColor: appBarColor,
-                  foregroundColor: appBarTextColor,
-                ))),
-        backgroundColor: backgroundColor,
-        drawer: const CustomNavigationDrawer(
-          active: "Edit Quiz",
-        ),
-        body: CustomForm(
-          onSubmit: _formSubmitted,
-          inputFieldsList: [
-            {
-              "label": "Question",
-              "controllerId": "question",
-              "color": Theme.of(context).primaryColor,
-            },
-            {
-              "label": "Answer",
-              "controllerId": "answer",
-              "color": Colors.green.shade800,
-            },
-            {
-              "label": "Option 1",
-              "controllerId": "option1",
-              "color": Colors.red.shade800,
-            },
-            {
-              "label": "Option 2",
-              "controllerId": "option2",
-              "color": Colors.red.shade800,
-            },
-            {
-              "label": "Option 3",
-              "controllerId": "option3",
-              "color": Colors.red.shade800,
-            },
-          ],
-        )
-        /* Button(
-          buttonText: 'Edit Quiz',
-          active: true,
-          onClick: (_) {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return (AddQuestionForm(
-                  onSubmit: (data) {
-                    print(data);
+                ),
+                leading: Builder(
+                  builder: (context) {
+                    return Container(
+                        margin: const EdgeInsets.only(left: 5),
+                        child: IconButton(
+                          icon: const Icon(Icons.menu),
+                          color: appBarTextColor,
+                          iconSize: 29,
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                        ));
                   },
-                  inputFieldsList: [
-                    {
-                      "label": "Question",
-                      "controllerId": "question",
-                      "color": Theme.of(context).primaryColor,
-                    },
-                    {
-                      "label": "Answer",
-                      "controllerId": "answer",
-                      "color": Colors.green.shade800,
-                    },
-                    {
-                      "label": "Option 1",
-                      "controllerId": "option1",
-                      "color": Colors.red.shade800,
-                    },
-                    {
-                      "label": "Option 2",
-                      "controllerId": "option2",
-                      "color": Colors.red.shade800,
-                    },
-                    {
-                      "label": "Option 3",
-                      "controllerId": "option3",
-                      "color": Colors.red.shade800,
-                    },
-                    {
-                      "label": "Option 4",
-                      "controllerId": "option4",
-                      "color": Colors.red.shade800,
-                    },
-                  ],
-                ));
-              },
-            );
-          },
-        ) */
-        );
+                ),
+                backgroundColor: appBarColor,
+                foregroundColor: appBarTextColor,
+              ))),
+      backgroundColor: backgroundColor,
+      drawer: const CustomNavigationDrawer(
+        active: "Edit Quiz",
+      ),
+      body: Stack(children: [
+        GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Container(
+            color: Colors.transparent,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
+        Column(
+          children: [
+            CustomForm(
+              inputFieldsList: [
+                {
+                  "label": "Question",
+                  "controllerId": "question",
+                  "controller": controllers['question'],
+                  "color": Theme.of(context).primaryColor,
+                },
+                {
+                  "label": "Answer",
+                  "controllerId": "answer",
+                  "controller": controllers['answer'],
+                  "color": Colors.green.shade800,
+                },
+                {
+                  "label": "Option 1",
+                  "controllerId": "option1",
+                  "controller": controllers['option1'],
+                  "color": Colors.red.shade800,
+                },
+                {
+                  "label": "Option 2",
+                  "controllerId": "option2",
+                  "controller": controllers['option2'],
+                  "color": Colors.red.shade800,
+                },
+                {
+                  "label": "Option 3",
+                  "controllerId": "option3",
+                  "controller": controllers['option3'],
+                  "color": Colors.red.shade800,
+                },
+              ],
+            ),
+            CustomElevatedButton(
+              buttonText: "Submit",
+              onClick: _formSubmitted,
+            )
+          ],
+        ),
+      ]),
+    );
   }
 }
--- lib/widgets/pages/new_quiz.dart
+++ lib/widgets/pages/new_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/main.dart';
+import 'package:quiz_app/widgets/base/custom_elevated_button.dart';
 import 'package:quiz_app/widgets/base/navigation_drawer.dart';
 import 'package:quiz_app/widgets/compound/custom_form.dart';
 
@@ -13,60 +14,126 @@ class NewQuiz extends StatefulWidget {
 }
 
 class _NewQuizState extends State<NewQuiz> {
-  void _questionSubmitted(data) {
+  final controllers = {
+    'question': TextEditingController(),
+    'answer': TextEditingController(),
+    'option1': TextEditingController(),
+    'option2': TextEditingController(),
+    'option3': TextEditingController()
+  };
+
+  void _formSubmitted(_) {
+    Map data = {};
+    List controllerKeys = controllers.keys.toList();
+    for (var i in controllerKeys) {
+      data[i] = controllers[i]!.text.trim();
+    }
+    for (var i in controllerKeys) {
+      controllers[i]!.clear(); // This loop clears all text fields
+    }
+    FocusManager.instance.primaryFocus?.unfocus();
     print(data);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(68),
-            child: Container(
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3.5),
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(68),
+          child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3.5),
+                ),
+              ]),
+              child: AppBar(
+                toolbarHeight: 68,
+                centerTitle: true,
+                title: const Text(
+                  "New Quiz",
+                  style: TextStyle(
+                    fontFamily: 'BeautifulPeople',
+                    fontSize: 25.25,
+                    letterSpacing: 1.3,
+                    wordSpacing: 1.2,
                   ),
-                ]),
-                child: AppBar(
-                  toolbarHeight: 68,
-                  centerTitle: true,
-                  title: const Text(
-                    "New Quiz",
-                    style: TextStyle(
-                      fontFamily: 'BeautifulPeople',
-                      fontSize: 25.25,
-                      letterSpacing: 1.3,
-                      wordSpacing: 1.2,
-                    ),
-                  ),
-                  leading: Builder(
-                    builder: (context) {
-                      return Container(
-                          margin: const EdgeInsets.only(left: 5),
-                          child: IconButton(
-                            icon: const Icon(Icons.menu),
-                            color: appBarTextColor,
-                            iconSize: 29,
-                            onPressed: () {
-                              Scaffold.of(context).openDrawer();
-                            },
-                          ));
-                    },
-                  ),
-                  backgroundColor: appBarColor,
-                  foregroundColor: appBarTextColor,
-                ))),
-        backgroundColor: backgroundColor,
-        drawer: const CustomNavigationDrawer(
-          active: "New Quiz",
+                ),
+                leading: Builder(
+                  builder: (context) {
+                    return Container(
+                        margin: const EdgeInsets.only(left: 5),
+                        child: IconButton(
+                          icon: const Icon(Icons.menu),
+                          color: appBarTextColor,
+                          iconSize: 29,
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                        ));
+                  },
+                ),
+                backgroundColor: appBarColor,
+                foregroundColor: appBarTextColor,
+              ))),
+      backgroundColor: backgroundColor,
+      drawer: const CustomNavigationDrawer(
+        active: "New Quiz",
+      ),
+      body: Stack(children: [
+        GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Container(
+            color: Colors.transparent,
+            width: double.infinity,
+            height: double.infinity,
+          ),
         ),
-        body: CustomForm(
-          onSubmit: _questionSubmitted,
-          inputFieldsList: const [],
-        ));
+        Column(
+          children: [
+            CustomForm(
+              inputFieldsList: [
+                {
+                  "label": "Question",
+                  "controllerId": "question",
+                  "controller": controllers['question'],
+                  "color": Theme.of(context).primaryColor,
+                },
+                {
+                  "label": "Answer",
+                  "controllerId": "answer",
+                  "controller": controllers['answer'],
+                  "color": Colors.green.shade800,
+                },
+                {
+                  "label": "Option 1",
+                  "controllerId": "option1",
+                  "controller": controllers['option1'],
+                  "color": Colors.red.shade800,
+                },
+                {
+                  "label": "Option 2",
+                  "controllerId": "option2",
+                  "controller": controllers['option2'],
+                  "color": Colors.red.shade800,
+                },
+                {
+                  "label": "Option 3",
+                  "controllerId": "option3",
+                  "controller": controllers['option3'],
+                  "color": Colors.red.shade800,
+                },
+              ],
+            ),
+            CustomElevatedButton(
+              buttonText: "Submit",
+              onClick: _formSubmitted,
+            )
+          ],
+        ),
+      ]),
+    );
   }
 }
